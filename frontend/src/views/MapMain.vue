21ebc30f48d2f61f2a02ed159b4740c0590ee839
조원빈 <cwb5116@naver.com>
2020-08-12 16:14:13 +0900

    S03P12A211-83 | frontend > MapMainDebugging

--- frontend/src/views/MapMain.vue
+++ frontend/src/views/MapMain.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <Map :items="this.items" /> -->
     <Map :items="this.$store.state.violationitems" />
   </div>
 </template>
@@ -15,28 +14,30 @@ export default {
   name: "MapMain",
   data() {
     return {
-      // items: [],
+      items: [],
     };
   },
   components: {
     Map,
   },
   created() {
-    http
-      .get(`/violation/${this.$session.get("userNo")}`)
-      .then(({ data }) => {
-        if (data) {
-          // this.local_violationitems = data;
-          // this.items = data;
-          this.$store.state.violationitems = data;
-          // alert("ㅔㅗ");
-        } else {
-          alert(" 실패했습니다.");
-        }
-      })
-      .catch(() => {
-        alert("에러가 발생했습니다.");
-      });
+    if (this.$session.get("email")) {
+      http
+        .get(`/violation/${this.$session.get("userNo")}`)
+        .then(({ data }) => {
+          if (data) {
+            // this.local_violationitems = data;
+            // this.items = data;
+            this.$store.state.violationitems = data;
+            // alert("ㅔㅗ");
+          } else {
+            alert(" 실패했습니다.");
+          }
+        })
+        .catch(() => {
+          alert("에러가 발생했습니다.");
+        });
+    }
   },
 };
 </script>
